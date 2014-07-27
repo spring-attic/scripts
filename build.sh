f69701c4b9323b0c060ef654d26013abc4bdf44e
Dave Syer <dsyer@gopivotal.com>
2014-07-27 07:13:36 -0700

    Use Maven reactor for build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,5 @@
 
 set -e
 
-TMPHOME=$(cd `dirname "$0"` && pwd)
-
-PLATFORM_HOME=${PLATFORM_HOME:-$TMPHOME}
-echo Home: $PLATFORM_HOME
-
-for f in */pom.xml; do
-  (cd ${f%*/pom.xml}; mvn clean install -DskipTests)
-done
+git submodule update --init
+mvn clean install
