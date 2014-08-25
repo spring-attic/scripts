ffd945a9d74585090922620e08e65ff6cafd09d5
Ryan Morgan <rmorgan@gopivotal.com>
2014-08-25 11:58:43 -0700

    The stores and customers application need APPLICATION_DOMAIN set when running with a non-standard application domain. (Running under PCF for example)

--- demo_deploy.sh
+++ demo_deploy.sh
@@ -34,6 +34,7 @@ function deploy_app() {
     cf push $APP -m 512m -p $DEMO_HOME/rest-microservices-$NAME/target/*.jar --no-start
     cf env $APP | grep SPRING_PROFILES_ACTIVE || cf set-env $APP SPRING_PROFILES_ACTIVE cloud
     [ "$PREFIX" != "" ] && cf env $APP | grep PREFIX || cf set-env $APP PREFIX $PREFIX
+    [ "$APPLICATION_DOMAIN" != "cfapps.io" ] && cf set-env $APP APPLICATION_DOMAIN $APPLICATION_DOMAIN
     
     cf bind-service $APP ${PREFIX}configserver
     cf bind-service $APP ${PREFIX}eureka
