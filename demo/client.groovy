a9dad74cb8dcd489981c5a196313b09fd308e568
Dave Syer <dsyer@pivotal.io>
2014-09-10 18:37:22 -0500

    Update to latest submodules

--- demo/client.groovy
+++ demo/client.groovy
@@ -1,9 +1,3 @@
-@GrabResolver('https://raw.githubusercontent.com/dickerpulli/maven-repo/master')
-@Grab('de.codecentric:spring-boot-starter-admin-client:1.0.1.RELEASE')
-@RestController
-class App { 
-  @RequestMapping('/')
-  def home() { 
-    [message: 'Hello World!']
-  }
+@EnableEurekaClient
+public class Application {
 }
