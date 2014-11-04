91fd5f440d47c13c3ca0134eea397a678066fff2
Dave Syer <dsyer@pivotal.io>
2014-11-04 07:33:00 +0000

    Update branch in build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 set -e
 
 git submodule update --init
-git submodule foreach '(git checkout master && git pull --rebase) || echo "Not attempting to sync"'
+git submodule foreach '(git checkout 1.0.0.M2 && git pull --rebase) || echo "Not attempting to sync"'
 mvn --settings config/.settings.xml clean install "$@"
