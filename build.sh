b93cfe8178b2b7bdd32dc2e7f2da529f218aad0e
Dave Syer <dsyer@pivotal.io>
2015-06-26 09:25:13 +0100

    Fix build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ git submodule update --init
 git submodule foreach '(git checkout master && git pull --rebase) || echo "Not attempting to sync"'
 if [ ".$@" == "." ]; then
     mvn --settings config/.settings.xml clean install -P build,!base,!starters,!apps -DskipTests
-    mvn --settings config/.settings.xml clean install -P !build,!base,starters,!apps -DskipTests
+    mvn --settings config/.settings.xml clean install -P !build,base,starters,!apps -DskipTests
 fi
 mvn --settings config/.settings.xml clean install "$@" -DskipTests
