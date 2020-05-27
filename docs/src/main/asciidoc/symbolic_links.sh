f8c6c043a5f1fbb27c39652201c3ea63c4dc42da
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-05-27 14:43:50 +0200

    Removed full path for symbolic links

--- docs/src/main/asciidoc/symbolic_links.sh
+++ docs/src/main/asciidoc/symbolic_links.sh
@@ -6,19 +6,21 @@ projectRoot="${1}"
 releaseTrainVersion="${2}"
 releaseTrainVersion="${releaseTrainVersion//./-}"
 
-rm -f "${projectRoot}"/src/main/asciidoc/index.adoc
-rm -f "${projectRoot}"/src/main/asciidoc/index.htmlsingleadoc
-rm -f "${projectRoot}"/src/main/asciidoc/index.pdfadoc
+rm -f index.adoc
+rm -f index.htmlsingleadoc
+rm -f index.pdfadoc
 
-echo "Making symbolic link for the main docs"
-ln -fs "${projectRoot}"/src/main/asciidoc/_spring-cloud-"${releaseTrainVersion}".adoc "${projectRoot}"/src/main/asciidoc/index.adoc
-
-if [ -f "${projectRoot}"/src/main/asciidoc/_spring-cloud-"${releaseTrainVersion}"-single.adoc ]; then
-    echo "Making symbolic link for the single page docs"
-    ln -fs "${projectRoot}"/src/main/asciidoc/_spring-cloud-"${releaseTrainVersion}"-single.adoc "${projectRoot}"/src/main/asciidoc/index.htmlsingleadoc
-fi
-
-if [ -f "${projectRoot}"/src/main/asciidoc/_spring-cloud-"${releaseTrainVersion}"-pdf.adoc ]; then
-    echo "Making symbolic link for the pdf docs"
-    ln -fs "${projectRoot}"/src/main/asciidoc/_spring-cloud-"${releaseTrainVersion}"-pdf.adoc "${projectRoot}"/src/main/asciidoc/index.pdfadoc
-fi
+cd "${projectRoot}/docs/src/main/asciidoc/"
+  echo "Making symbolic link for the main docs"
+  ln -fs _spring-cloud-"${releaseTrainVersion}".adoc index.adoc
+  
+  if [ -f _spring-cloud-"${releaseTrainVersion}"-single.adoc ]; then
+      echo "Making symbolic link for the single page docs"
+      ln -fs _spring-cloud-"${releaseTrainVersion}"-single.adoc index.htmlsingleadoc
+  fi
+  
+  if [ -f _spring-cloud-"${releaseTrainVersion}"-pdf.adoc ]; then
+      echo "Making symbolic link for the pdf docs"
+      ln -fs _spring-cloud-"${releaseTrainVersion}"-pdf.adoc index.pdfadoc
+  fi
+cd "${projectRoot}"
